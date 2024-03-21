fd13816b498846680c477ddc6893ef46477e53d3
shadow140804 <149602112+shadow140804@users.noreply.github.com>
2024-03-21 20:50:22 +0530

    Add files via upload

---
+++ Book my project-clone/js-2/data.js
@@ -0,0 +1,20 @@
+let movies = [
+    {
+        image: 'img/banner/holi.jpg'
+    },
+    {
+        image: 'img/banner/banner2.webp'
+    },
+    {
+        image: 'img/banner/banner3.webp'
+    },
+    {
+        image: 'img/banner/banner4.webp'
+    },
+    {
+        image: 'img/banner/banner5.webp'
+    },
+    {
+        image: 'img/banner/banner6.webp'
+    }
+]
